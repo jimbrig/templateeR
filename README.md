37ed80bc6d641bb7ddf6d7bb1045f38bb04c1880
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2022-01-22 19:44:06 -0500

    config: add R package infrastructure

---
+++ README.md
@@ -0,0 +1,21 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# templateeR
+
+<!-- badges: start -->
+
+[![R-CMD-check](https://github.com/jimbrig/templateeR/workflows/R-CMD-check/badge.svg)](https://github.com/jimbrig/templateeR/actions)
+<!-- badges: end -->
+
+The goal of templateeR is to …
+
+## Installation
+
+You can install the development version of templateeR from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("jimbrig/templateeR")
+```
---
+++ inst/scripts/pkgdevt.R
@@ -0,0 +1,69 @@
+
+#  ------------------------------------------------------------------------
+#
+# Title : templateeR R Package Development Script
+#    By : Jimmy Briggs
+#  Date : 2022-01-22
+#
+#  ------------------------------------------------------------------------
+
+# library R packages ------------------------------------------------------
+
+library(devtools)
+library(usethis)
+library(pkgbuild)
+library(pkgload)
+library(pkgdown)
+library(testthat)
+library(knitr)
+library(pak)
+library(purrr)
+library(desc)
+library(chameleon)
+library(attachment)
+
+
+# initialize package ------------------------------------------------------
+
+usethis::create_package("templateeR")
+usethis::use_namespace()
+usethis::use_roxygen_md()
+usethis::use_git()
+usethis::use_package_doc()
+usethis::use_tibble() # #' @return a [tibble][tibble::tibble-package]
+usethis::use_pipe()
+usethis::use_tidy_eval()
+devtools::document()
+
+
+# github ------------------------------------------------------------------
+
+# set description and title first so included in GH repo
+desc::desc_set(
+  "Title" = "Collection of templates for R",
+  "Description" = "Apply variouse R related templates."
+)
+
+usethis::use_github()
+
+# data --------------------------------------------------------------------
+
+usethis::use_data_raw("gh-labels")
+
+
+
+# inst directories --------------------------------------------------------
+
+
+# functions ---------------------------------------------------------------
+
+c(
+  "gh-labels",
+  "utils"
+) |>
+  purrr::walk(usethis::use_r, open = FALSE)
+
+
+# tests -------------------------------------------------------------------
+
+usethis::use_test("gh_labels")
