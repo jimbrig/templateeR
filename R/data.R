249490d997382cf8218ae595d42d6411dbf3d2b5
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2022-01-22 19:44:26 -0500

    doc: document gh_labels dataset

---
+++ R/data.R
@@ -0,0 +1,15 @@
+
+#' Default GitHub Issue Labels
+#'
+#' This `data.frame` houses my default repository issue labels applied to R projects
+#' via [usethis::use_github_labels()].
+#'
+#' @seealso [use_gh_labels()], [usethis::use_github_labels()].
+#'
+#' @format A data frame with 9 rows and 3 variables:
+#' \describe{
+#'   \item{\code{label}}{character. Label names.}
+#'   \item{\code{description}}{character. Label descriptions.}
+#'   \item{\code{colour}}{character. Label colors represented in hexadecimal format.}
+#' }
+"gh_labels"
